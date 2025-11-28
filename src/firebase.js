b7e69c326edfed878ceb4170b5daf6fe938911da
SS7ZX <predators.king.973@gmail.com>
2025-11-28 07:28:34 +0700

    Final Push LifeFin With DataBase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEBJOtvbESS5aQOTEQI0jrfauqeTtORts",
+  authDomain: "lifefin.firebaseapp.com",
+  projectId: "lifefin",
+  storageBucket: "lifefin.firebasestorage.app",
+  messagingSenderId: "662536262300",
+  appId: "1:662536262300:web:c40307637655a9cb481359"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
